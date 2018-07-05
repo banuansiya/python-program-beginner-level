b94caa3bbbbe0d3863c5ff54525bf1c1dba480f1
ansiyabanu <39331136+banuansiya@users.noreply.github.com>
2018-07-05 16:45:33 +0530

    Add files via upload

---
+++ guvi1.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class p
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=Integer.parseInt(System.console().readLine());
+		if(n>0)
+		System.out.println("positive");
+		else if(n<0)
+		System.out.println("negative");
+		else if(n==0)
+		System.out.println("zero");
+		else
+		System.out.println("enter correct number");
+
+
+
+
+		// your code goes here
+	}
+}
