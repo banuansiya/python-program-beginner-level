2d1ebf8ab3477e1c0a176f3f39d3667452e50110
ansiyabanu <39331136+banuansiya@users.noreply.github.com>
2018-07-05 17:03:36 +0530

    Add files via upload

---
+++ guvi2.java
@@ -0,0 +1,19 @@
+class p
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=Integer.parseInt(System.console().readLine());
+		if(n%2==0)
+		System.out.println("even");
+		else if(n%2!=0)
+		System.out.println("odd");
+
+		else
+		System.out.println("invalid");
+
+
+
+
+		// your code goes here
+	}
+}
